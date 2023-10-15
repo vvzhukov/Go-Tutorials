2d3e0dfe70ab5e197af593059b0bdb31bbc2c945
vzhukov <zhuko.v.italy@gmail.com>
2023-10-14 20:51:55 -0500

    module 1 samples added

--- 0-Hello.go
+++ 0-Hello.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import ( //factored import statement
+	"fmt"
+	"math"
+)
+
+var ( //factored variables declaration
+	ToBe   bool   = false
+	MaxInt uint64 = 1<<64 - 1
+)
+
+func add(x, y int) int {
+	return x + y
+}
+
+// functions with multiple results
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+// functions with 'naked' return
+// only use in short functions
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+// variables at package level
+var d, e, f bool = false, true, false
+
+// we may omit var type when var is initialized
+var d2, e2, f2 = false, "string type", false
 
 func main() {
-	fmt.Println("Hello, VZ")
+	// variables at function level
+	var g int
+
+	// implicit var declaration
+	k := 2 // only inside functions
+
+	fmt.Println(d, e, f, g, d2, e2, f2)
+	fmt.Println(k)
+	a, b := swap("Hello", "VZ")
+	fmt.Println(b, a)
+	fmt.Println("Custom function call: ", add(42, 3))
+	fmt.Println("Exported names should be capitalized: ", math.Pi)
+	fmt.Println(split(17))
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vvzhukov/Go-Tutorials
+
+go 1.21.3
